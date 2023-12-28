3257eedbc057a15cbe86ab412d2cefe7386d42d5
StVadimBir <128745468+StVadimBir@users.noreply.github.com>
2023-12-27 20:34:13 -0600

    Base Code

---
+++ src/main/java/org/example/CaesarCipherDriver.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class CaesarCipherDriver {
+
+    public static void main(String[] args) {
+
+        // Step 1: Get plaintext input
+        // Core Here
+
+        // Step 2: Check the input length
+        // Core Here
+
+        // Step 2: Get seed for random number generation
+        // Core Here
+
+        // Step 3: Generate the key
+        // Core Here
+
+        // Step 4: Encode the plaintext
+        // Core Here
+
+        // Step 5: Display the ciphertext
+        // Core Here (Output must be as required)
+    }
+}
+
